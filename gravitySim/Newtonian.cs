eccadaddbccdd7240e0c1277dbb0917f6e7db001
David <dawoud2003@gmail.com>
2025-06-14 18:57:27 +0200

    Cleaned the code a little

--- gravitySim/Newtonian.cs
+++ gravitySim/Newtonian.cs
@@ -11,11 +11,6 @@ public class NewtonianSim
         CelestialObject earth = new CelestialObject(5.972E+24, 6378, new VectorSpherical(0, 2 * Math.PI / 31536000, 0).ToCartesian(), new VectorSpherical(149597870700, 0, 0).ToCartesian());
         CelestialObject sun = new CelestialObject(1.988416E+30, 695700, new VectorCartesian(0, 0, 0), new VectorCartesian(0, 0, 0));
         Scene solarSystem = new Scene(new List<CelestialObject> { earth, sun });
-
-        VectorCartesian vec1 = new VectorCartesian(1, 2, 3);
-        VectorCartesian vec2 = new VectorCartesian(4, 5, 6);
-        double prod = vec1 * vec2;
-        Console.WriteLine(prod);
     }
 }
 
@@ -25,10 +20,10 @@ public class Scene
     public double G = 6.6743E-11;
     public VectorCartesian Force(VectorCartesian pos1, VectorCartesian pos2, double m1, double m2)
     {
-        double dist2 = Math.Pow((pos1.X - pos2.X), 2) + Math.Pow((pos1.Y - pos2.Y), 2) + Math.Pow((pos1.Z - pos2.Z), 2);
+        double dist2 = (pos2 - pos1).Norm(pos2 - pos1);
         VectorCartesian direction = pos2 - pos1;
         double norm = direction.Norm(direction);
-        double force = (G * m1 * m2) / dist2;
+        double force = G * m1 * m2 / dist2;
         return force / norm * direction;
     }
 
